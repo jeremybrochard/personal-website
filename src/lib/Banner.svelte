4f63d66ff66d410ff1286e921b1a2972449cfcfc
celineung <celine.ung.pro@gmail.com>
2024-05-09 23:54:13 +0200

    feat: add analytics

--- src/lib/Banner.svelte
+++ src/lib/Banner.svelte
@@ -10,12 +10,12 @@
     <h2>Développeuse web fullstack</h2>
     <p class="description">
       Consultante web fullstack depuis 2015, j’accompagne mes clients dans la réalisation de leurs projets web. J'ai récemment rejoint
-      <a href="https://aycandoo.fr/" target="_blank" rel="noopener noreferrer">AYCANDO</a>.
+      <a href="https://aycandoo.fr/" target="_blank" rel="noopener noreferrer" id="aycandoo">AYCANDO</a>.
     </p>
     <p class="social-medias">
-      <a href="https://www.linkedin.com/in/celine-ung" target="_blank" rel="noopener noreferrer"><img src={LinkedInIcon} alt="Profile LinkedIn de Céline"></a>
-      <a href="https://github.com/celineung" target="_blank" rel="noopener noreferrer"><img src={GithubIcon} alt="Profile Github de Céline"></a>
-      <a href="mailto:celine.ung.pro@gmail.com?subject=Prise de contact">
+      <a href="https://www.linkedin.com/in/celine-ung" target="_blank" rel="noopener noreferrer" id="cu-linkedin"><img src={LinkedInIcon} alt="Profile LinkedIn de Céline"></a>
+      <a href="https://github.com/celineung" target="_blank" rel="noopener noreferrer" id="cu-github"><img src={GithubIcon} alt="Profile Github de Céline"></a>
+      <a href="mailto:celine.ung.pro@gmail.com?subject=Prise de contact" id="cu-contact">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="2 2 21 20" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" role="img" aria-label="Prise de contact par mail">
           <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
         </svg>

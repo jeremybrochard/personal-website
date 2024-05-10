11da902df04bbc12d7fa6b080946eed08ad41452
celineung <celine.ung.pro@gmail.com>
2024-05-10 16:35:08 +0200

    refactior: banner wordings

--- src/lib/Banner.svelte
+++ src/lib/Banner.svelte
@@ -9,13 +9,16 @@
     <h1>Céline Ung</h1>
     <h2>Développeuse web fullstack</h2>
     <p class="description">
-      Consultante web fullstack depuis 2015, j’accompagne mes clients dans la réalisation de leurs projets web. J'ai récemment rejoint
+      Depuis 2015, j’accompagne mes clients dans la réalisation de leurs projets web. J'ai récemment rejoint
       <a href="https://aycandoo.fr/" target="_blank" rel="noopener noreferrer" id="aycandoo">AYCANDO</a>.
+    </p>
+    <p class="description">
+      Vous avez une idée ou un projet de site ou d'application web ? Je peux vous accompagner aussi bien dans sa réalisation que sa maintenance.<br><a href="https://aycandoo.fr/contact/" target="_blank" rel="noopener noreferrer"  class="cu-contact">Prenez contact avec notre équipe AYCANDOO</a>.
     </p>
     <p class="social-medias">
       <a href="https://www.linkedin.com/in/celine-ung" target="_blank" rel="noopener noreferrer" id="cu-linkedin"><img src={LinkedInIcon} alt="Profile LinkedIn de Céline"></a>
       <a href="https://github.com/celineung" target="_blank" rel="noopener noreferrer" id="cu-github"><img src={GithubIcon} alt="Profile Github de Céline"></a>
-      <a href="mailto:celine.ung.pro@gmail.com?subject=Prise de contact" id="cu-contact">
+      <a href="https://aycandoo.fr/contact/" target="_blank" rel="noopener noreferrer" class="cu-contact">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="2 2 21 20" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" role="img" aria-label="Prise de contact par mail">
           <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
         </svg>
@@ -43,11 +46,15 @@
 
   .container {
     margin: auto;
-    padding: 7rem var(--container-padding) 0 var(--container-padding);
+    padding: 2rem var(--container-padding) 0 var(--container-padding);
     max-width: var(--container-max-width);
     display: flex;
     align-items: center;
     flex-direction: column;
+
+    @media (min-width: 768px) {
+      padding-top: 7rem;
+    }
 
     span {
       font-size: 0.9rem;
@@ -94,8 +101,13 @@
 
     &.navigate-down {
       @extend %icon;
-      margin-top: 6rem;
+      margin-top: 2rem;
       margin-bottom: 3rem;
+
+
+      @media (min-width: 768px) {
+        margin-top: 6rem;
+      }
     }
   }
 </style>

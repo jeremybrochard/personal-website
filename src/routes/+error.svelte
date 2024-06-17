26c5f3703a4cd4e34294e152340e5142b5bfa04d
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-06-17 18:03:30 +0200

    refactor: add translations on error page and update styles

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,32 +1,26 @@
 <script lang="ts">
-  import { page } from '$app/stores';
+	import { page } from '$app/stores';
+	import { _ } from 'svelte-i18n';
 </script>
 
-<section>
-  {#if $page.status === 404}
-    <h1>La page demandée n'existe pas</h1>
-    <p>L'url saisie est-elle correcte ?</p>
-  {:else if $page.error}
-    <p>{$page.status} {$page.error.message}</p>
-  {:else}
-    <p>{$page.status}</p>
-    <p>Une erreur est survenue.</p>
-  {/if}
-  <a href="/">Retourner sur la page d'accueil</a>
+<section class="flex flex-col items-center gap-4 py-24">
+	{#if $page.status === 404}
+		<h1 class="my-1 mx-0 text-2xl font-semibold">{$_('errors.404.title')}</h1>
+		<p>{$_('errors.404.description')}</p>
+	{:else if $page.error}
+		<h1 class="my-1 mx-0 text-2xl font-semibold">{$page.status}</h1>
+		<p>{$page.error.message}</p>
+	{:else}
+		<h1 class="my-1 mx-0 text-2xl font-semibold">{$page.status}</h1>
+		<p>{$_('errors.unknown.description')}</p>
+	{/if}
+	<a href="/" class="no-underline text-secondary hover:underline hover:grayscale-70"
+		>{$_('errors.goBack')}</a
+	>
 </section>
 
 <style lang="scss">
-  section {
-    text-align: center;
-
-    a {
-      text-decoration: none;
-      color: var(--secondary-color);
-
-      &:hover {
-        text-decoration: underline;
-        filter: grayscale(0.7);
-      }
-    }
-  }
+	section {
+		min-height: calc(100vh - 6rem);
+	}
 </style>

458da22007671594a5eee60ca2ec4e4a4e6b3fe6
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-06-17 18:01:37 +0200

    refactor: update page layout to have a header and a footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script>
 	import Banner from '$lib/Banner.svelte';
-	import Credits from '$lib/Credits.svelte';
 	import References from '$lib/References.svelte';
 </script>
 
 <Banner />
 <References />
-<Credits />

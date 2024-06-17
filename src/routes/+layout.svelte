209e8b4416dc58368a96cb140ff9292f751c0431
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-06-17 16:17:24 +0200

    fix: wait for locale to be loaded before displaying web pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,19 @@
-<script>
+<script lang="ts">
 	import Navigation from '$lib/Navigation.svelte';
 	import Seo from '$lib/Seo.svelte';
+	import { isLoading } from 'svelte-i18n';
 	import '../lib/i18n';
 	import './styles.css';
 </script>
 
-<Seo />
-<Navigation />
+{#if $isLoading}
+  Please wait...
+{:else}
+  <Seo />
+  <Navigation />
+  
+  <main>
+    <slot />
+  </main>
+{/if}
 
-<main>
-	<slot />
-</main>

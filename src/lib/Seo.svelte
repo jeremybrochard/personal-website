b4167747f4696912530a0779c9bda438fbc89266
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-06-17 16:49:47 +0200

    fix: set html lang back to fr by default

--- src/lib/Seo.svelte
+++ src/lib/Seo.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import { _, locale } from 'svelte-i18n';
+	import { _ } from 'svelte-i18n';
 </script>
 
 <svelte:head>
-	<html lang={$locale} />
 	<title>{$_('pageTitle')}</title>
 	<meta name="description" content={$_('seo.description')} />
 	<meta property="og:description" content={$_('seo.ogDescription')} />

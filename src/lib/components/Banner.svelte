2c1efa09ec1d6df945546dff9524746c988fc7fc
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-06-19 15:44:47 +0200

    feat: add stars parallax effect on banner

--- src/lib/components/Banner.svelte
+++ src/lib/components/Banner.svelte
@@ -17,8 +17,13 @@
 	}
 </script>
 
-<section class="bg-primary m-auto text-white">
-	<div class="flex flex-col items-center m-auto pt-8 pb-0 px-6 max-w-6xl md:pt-28">
+<section class="bg-primary text-white">
+	<div class="stars"></div>
+  <div class="stars2"></div>
+  <div class="stars3"></div>
+	<div
+		class="max-w-6xl mx-auto flex flex-col items-center pt-8 pb-0 px-6 md:pt-28"
+	>
 		<div>{$_('pageTitlePrefix')}</div>
 		<h1 class="my-1 mx-0 text-secondary text-2xl font-semibold">{$_('pageTitle')}</h1>
 		<h2 class="my-8 mx-0 text-center text-4xl font-semibold">
@@ -86,3 +91,81 @@
 		</a>
 	</div>
 </section>
+
+<style lang="scss">
+	@function multiple-box-shadow($n) {
+		$value: '#{random(2000)}px #{random(2000)}px #FFF';
+		@for $i from 2 through $n {
+			$value: '#{$value} , #{random(3000)}px #{random(1500)}px #FFF';
+		}
+
+		@return unquote($value);
+	}
+
+	$shadows-small: multiple-box-shadow(700);
+	$shadows-medium: multiple-box-shadow(200);
+	$shadows-big: multiple-box-shadow(100);
+
+	.stars {
+		width: 1px;
+		height: 1px;
+		background: transparent;
+		box-shadow: $shadows-small;
+		animation: animStar 50s linear infinite;
+
+		&:after {
+			content: ' ';
+			position: absolute;
+			top: 2000px;
+			width: 1px;
+			height: 1px;
+			background: transparent;
+			box-shadow: $shadows-small;
+		}
+	}
+
+  .stars2 {
+		width: 2px;
+		height: 2px;
+		background: transparent;
+		box-shadow: $shadows-medium;
+		animation: animStar 100s linear infinite;
+
+		&:after {
+			content: ' ';
+			position: absolute;
+			top: 2000px;
+			width: 2px;
+			height: 2px;
+			background: transparent;
+			box-shadow: $shadows-medium;
+		}
+	}
+
+  .stars3 {
+		width: 3px;
+		height: 3px;
+		background: transparent;
+		box-shadow: $shadows-big;
+		animation: animStar 150s linear infinite;
+
+		&:after {
+			content: ' ';
+			position: absolute;
+			top: 2000px;
+			width: 3px;
+			height: 3px;
+			background: transparent;
+			box-shadow: $shadows-big;
+		}
+	}
+
+	@keyframes animStar {
+		from {
+			transform: translateY(0px);
+		}
+		to {
+			transform: translateY(-2000px);
+		}
+	}
+</style>

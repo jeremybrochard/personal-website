eebc78576ffe8a77d48d0b868b2055ca8531762d
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-10-15 11:50:27 +0200

    refactor: move tech-stack components in their own folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Banner from '$lib/components/Banner.svelte';
 	import References from '$lib/components/References.svelte';
-	import TechStack from '$lib/components/TechStack.svelte';
+	import TechStack from '$lib/components/tech-stack/TechStack.svelte';
 </script>
 
 <Banner />

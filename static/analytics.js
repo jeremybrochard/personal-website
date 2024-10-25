9e31cfbda1700c14bd7092ab24162495d178edd3
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-10-25 18:19:39 +0200

    fix: add analytics init in specific file instead of inline script

---
+++ static/analytics.js
@@ -0,0 +1,8 @@
+// @ts-ignore
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  // @ts-ignore
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-S9P4HSSWE1');
